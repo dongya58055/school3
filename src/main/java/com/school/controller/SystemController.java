4941af9e12c40470f040b4c2cfe6e4775bacf192
dong <dong58055@gmail.com>
2025-06-05 16:15:15 +0900

    完结

--- src/main/java/com/school/controller/SystemController.java
+++ src/main/java/com/school/controller/SystemController.java
@@ -12,6 +12,7 @@ import javax.imageio.ImageIO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.dto.LoginForm;
 import com.school.eneity.Admin;
 import com.school.eneity.Student;
@@ -29,6 +31,7 @@ import com.school.service.StudentService;
 import com.school.service.TeacherService;
 import com.util.CreateVerifiCodeImage;
 import com.util.JwtHelper;
+import com.util.MD5;
 import com.util.Result;
 import com.util.ResultCodeEnum;
 
@@ -76,10 +79,9 @@ public class SystemController {
 		// 获取图片的验证码
 //		char[] code = CreateVerifiCodeImage.getVerifiCode();
 //		String string = code.toString();     
-		/**code 是一个对象（数组是对象）。
-		 * toString() 方法调用的是 Object.toString()，默认返回的是“类名@哈希码”的形式，比如 [C@4eec7777：
-			[C 代表它是 char[] 类型。
-			@4eec7777 是内存地址的哈希值。
+		/**
+		 * code 是一个对象（数组是对象）。 toString() 方法调用的是 Object.toString()，默认返回的是“类名@哈希码”的形式，比如
+		 * [C@4eec7777： [C 代表它是 char[] 类型。 @4eec7777 是内存地址的哈希值。
 		 */
 		String string = new String(CreateVerifiCodeImage.getVerifiCode());
 		// 验证码文本放入session，给验证做准备
@@ -224,12 +226,11 @@ public class SystemController {
 			break;
 		}
 		}
-		
+
 		return Result.ok(map);
 
 	}
 
-	
 	/**
 	 * 描述:图片上传
 	 * 
@@ -238,48 +239,103 @@ public class SystemController {
 	 *                           MultipartFile 对象，供我们在控制器中处理。
 	 * @RequestPart 是 Spring 中用于处理 multipart/form-data 请求中 非表单字段（如文件）或嵌套 JSON 数据 的注解
 	 * @return 返回值:
-	 * @throws IOException 
-	 * @throws IllegalStateException 
+	 * @throws IOException
+	 * @throws IllegalStateException
 	 * @exception 异常:
 	 */
-	
+
 	@Value("${school.basepath}")
 	private String path;
-	
-	//上传图片
+
+	// 上传图片
 	@PostMapping("/headerImgUpload")
-	public Result upload (@RequestPart("multipartFile") MultipartFile multipartFile) throws IllegalStateException, IOException {
-		//保存文件
-		//使用uuid重新生成文件名
+	public Result upload(@RequestPart("multipartFile") MultipartFile multipartFile)
+			throws IllegalStateException, IOException {
+		// 保存文件
+		// 使用uuid重新生成文件名
 		String name = UUID.randomUUID().toString().replace("-", "").toLowerCase();
 		String originalFilename = multipartFile.getOriginalFilename();
-		//获取后缀
+		// 获取后缀
 		int lastIndexOf = originalFilename.lastIndexOf(".");
 		// 获取后缀名
 		String lastname = originalFilename.substring(originalFilename.lastIndexOf("."));
 		// 文件新名称
 		String fileName = name + lastname;
 		System.out.println(fileName);
-		//响应图片路径
-		String filePath=path.concat(fileName);
+		// 响应图片路径
+		String filePath = path.concat(fileName);
 		System.out.println(filePath);
-		//保存图片到本地
+		// 保存图片到本地
 		multipartFile.transferTo(new File(filePath));
 		// 返回图片路径
 		return Result.ok("upload/".concat(fileName));
-		
+
 	}
-	
+
 	/**
-	 * 描述:
+	 * 描述:更改密码
+	 * 
 	 * @param 参数说明:
 	 * @return 返回值:
 	 * @exception 异常:
 	 */
-	@PostMapping("/")
-	public Result password() {
-		return null;
-		
+	@PostMapping("/updatePwd/{oldPwd}/{newPwd}")
+	public Result password(@RequestHeader("token") String token,@PathVariable String oldPwd, 
+		@PathVariable String newPwd) {
+		boolean expiration = JwtHelper.isExpiration(token);
+		if (expiration) {
+			// 已经过期
+			return Result.fail().message("token失效，请重新登录");
+		}
+		// 根据token获取用户类型
+		Integer userId = JwtHelper.getUserId(token).intValue();
+		Integer userType = JwtHelper.getUserType(token);
+		switch (userType) {
+		case 1: {
+			LambdaQueryWrapper<Admin> lqw = new LambdaQueryWrapper<>();
+			lqw.eq(Admin::getId, userId);
+			lqw.eq(Admin::getPassword, MD5.encrypt(oldPwd));
+			Admin admin = adminService.getOne(lqw);
+			if (admin != null) {
+				admin.setPassword(MD5.encrypt(newPwd));
+				adminService.saveOrUpdate(admin);
+			} else {
+				return Result.fail().message("原密码有误");
+			}
+			break;
+
+		}
+
+		case 2: {
+			LambdaQueryWrapper<Student> lqw = new LambdaQueryWrapper<>();
+			lqw.eq(Student::getId, userId);
+			lqw.eq(Student::getPassword, MD5.encrypt(oldPwd));
+			Student student = studentService.getOne(lqw);
+			if (student != null) {
+				student.setPassword(MD5.encrypt(newPwd));
+				studentService.saveOrUpdate(student);
+			} else {
+				return Result.fail().message("原密码有误");
+			}
+			break;
+		}
+
+		case 3: {
+			LambdaQueryWrapper<Teacher> lqw = new LambdaQueryWrapper<>();
+			lqw.eq(Teacher::getId, userId);
+			lqw.eq(Teacher::getPassword, MD5.encrypt(oldPwd));
+			Teacher teacher = teacherService.getOne(lqw);
+			if (teacher != null) {
+				teacher.setPassword(MD5.encrypt(newPwd));
+				teacherService.saveOrUpdate(teacher);
+			} else {
+				return Result.fail().message("原密码有误");
+			}
+			break;
+		}
+
+		}
+		return Result.ok();
+
 	}
-	
 }
